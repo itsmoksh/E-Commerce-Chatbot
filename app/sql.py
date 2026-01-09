8496ea27e9c8192ef9d7d8437dc8fec6103a52e3
Moksh Jain <mokshjain2005jain@gmail.com>
2026-01-09 15:43:40 +0530

    removing product_gen_llm from sql

--- app/sql.py
+++ app/sql.py
@@ -40,16 +40,6 @@ The query should have all the fields in SELECT clause (i.e. SELECT *)
 
 Just the SQL query is needed, nothing more. Always provide the SQL in between the <SQL></SQL> tags."""
 
-comprehension_prompt = """You are an expert in understanding the context of the question and replying based on the data pertaining to the question provided. You will be provided with Question: and Data:. The data will be in the form of an array or a dataframe or dict. Reply based on only the data provided as Data for answering the question asked as Question. Do not write anything like 'Based on the data' or any other technical words. Just a plain simple natural language response.
-The Data would always be in context to the question asked. For example is the question is “What is the average rating?” and data is “4.3”, then answer should be “The average rating for the product is 4.3”. So make sure the response is curated with the question and data. Make sure to note the column names to have some context, if needed, for your response.
-There can also be cases where you are given an entire dataframe in the Data: field. Always remember that the data field contains the answer of the question asked. All you need to do is to always reply in the following format when asked about a product: 
-Produt title, price in indian rupees, discount, and rating, and then product link. Take care that all the products are listed in list format, one line after the other. Not as a paragraph.
-For example:
-1. Campus Women Running Shoes: Rs. 1104 (35 percent off), Rating: 4.4 <link>
-2. Campus Women Running Shoes: Rs. 1104 (35 percent off), Rating: 4.4 <link>
-3. Campus Women Running Shoes: Rs. 1104 (35 percent off), Rating: 4.4 <link>
-
-"""
 
 def generate_sql_query(question):
     completion = groq_client.chat.completions.create(
@@ -68,23 +58,6 @@ def generate_sql_query(question):
 
     return completion.choices[0].message.content
 
-def data_compression(question,context):
-    completion = groq_client.chat.completions.create(
-        model=GROQ_MODEL,
-        messages=[
-            {"role": "system",
-             "content": comprehension_prompt
-             },
-            {"role":'user',
-             "content": f'Question: {question} Data: {context}'
-             }
-        ],
-        temperature=  0.2,
-        max_completion_tokens=32768
-    )
-
-    return completion.choices[0].message.content
-
 def sql_chain(question):
     query = generate_sql_query(question)
     pattern = '<SQL>(.*?)<\/SQL>'
@@ -99,7 +72,6 @@ def sql_chain(question):
         return "Sorry, there's a problem executing sql query"
 
     context = response.to_dict(orient='records')
-    print(context)
     results =  [(f'''{idx+1}. {r['title']}: Rs. {r['price']} ({round(r['discount']*100,0)} percent off), Rating: {r['avg_rating']} {r['product_link']}''') for idx,r in enumerate(context)]
     return results
 
